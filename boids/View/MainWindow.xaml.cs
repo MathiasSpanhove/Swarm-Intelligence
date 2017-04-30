61be87524d79cb7296cdefb5b8d2fe90804c52a5
Mathias S. <mathias.spanhove@hotmail.be>
2017-04-30 22:47:59 +0200

    Created Viewmodels + Pause option

--- boids/View/MainWindow.xaml.cs
+++ boids/View/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using ViewModel;
 
 namespace View
 {
@@ -26,19 +27,86 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
 
-            // WARNING: THIS CODE VIOLATES MVVM PRINCIPLES
-            // IT IS FOR ILLUSTRATIVE PURPOSES ONLY
-            this.Simulation = new Simulation();
-            this.Simulation.Species[0].CreateBoid(new Vector2D(50, 50));
-            this.Simulation.Species[1].CreateBoid(new Vector2D(150, 150));
-            this.DataContext = this;
+    public static class SizeObserver
+    {
+        public static readonly DependencyProperty ObserveProperty = DependencyProperty.RegisterAttached(
+            "Observe",
+            typeof(bool),
+            typeof(SizeObserver),
+            new FrameworkPropertyMetadata(OnObserveChanged));
 
-            // Using the timer like this will yield choppy animation
-            var timer = new DispatcherTimer(TimeSpan.FromMilliseconds(20), DispatcherPriority.Render, (x, y) => { this.Simulation.Update(0.02); }, this.Dispatcher);
-            timer.Start();
+        public static readonly DependencyProperty ObservedWidthProperty = DependencyProperty.RegisterAttached(
+            "ObservedWidth",
+            typeof(double),
+            typeof(SizeObserver));
+
+        public static readonly DependencyProperty ObservedHeightProperty = DependencyProperty.RegisterAttached(
+            "ObservedHeight",
+            typeof(double),
+            typeof(SizeObserver));
+
+        public static bool GetObserve(FrameworkElement frameworkElement)
+        {
+            return (bool)frameworkElement.GetValue(ObserveProperty);
         }
 
-        public Simulation Simulation { get; }
+        public static void SetObserve(FrameworkElement frameworkElement, bool observe)
+        {
+            frameworkElement.SetValue(ObserveProperty, observe);
+        }
+
+        public static double GetObservedWidth(FrameworkElement frameworkElement)
+        {
+            return (double)frameworkElement.GetValue(ObservedWidthProperty);
+        }
+
+        public static void SetObservedWidth(FrameworkElement frameworkElement, double observedWidth)
+        {
+            frameworkElement.SetValue(ObservedWidthProperty, observedWidth);
+        }
+
+        public static double GetObservedHeight(FrameworkElement frameworkElement)
+        {
+            return (double)frameworkElement.GetValue(ObservedHeightProperty);
+        }
+
+        public static void SetObservedHeight(FrameworkElement frameworkElement, double observedHeight)
+        {
+            frameworkElement.SetValue(ObservedHeightProperty, observedHeight);
+        }
+
+        private static void OnObserveChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var frameworkElement = (FrameworkElement)dependencyObject;
+
+            if ((bool)e.NewValue)
+            {
+                frameworkElement.SizeChanged += OnFrameworkElementSizeChanged;
+                UpdateObservedSizesForFrameworkElement(frameworkElement);
+            }
+            else
+            {
+                frameworkElement.SizeChanged -= OnFrameworkElementSizeChanged;
+            }
+        }
+
+        private static void OnFrameworkElementSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateObservedSizesForFrameworkElement((FrameworkElement)sender);
+        }
+
+        private static void UpdateObservedSizesForFrameworkElement(FrameworkElement frameworkElement)
+        {
+            // WPF 4.0 onwards
+            frameworkElement.SetCurrentValue(ObservedWidthProperty, frameworkElement.ActualWidth);
+            frameworkElement.SetCurrentValue(ObservedHeightProperty, frameworkElement.ActualHeight);
+
+            // WPF 3.5 and prior
+            ////SetObservedWidth(frameworkElement, frameworkElement.ActualWidth);
+            ////SetObservedHeight(frameworkElement, frameworkElement.ActualHeight);
+        }
     }
 }
---
+++ boids/ViewModel/MainViewModel.cs
@@ -0,0 +1,199 @@
+﻿using Bindings;
+using Cells;
+using Mathematics;
+using Model;
+using Model.AI;
+using Model.Species;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+
+            this.simulation = new Simulation();
+            CreateHunter(50, 50);
+            CreatePrey(150, 150);
+            CreatePrey(20, 20);
+
+            this.timer = new DispatcherTimer(TimeSpan.FromMilliseconds(20), DispatcherPriority.Render, (x, y) =>
+            {
+                Update(0.02);
+            }, _dispatcher);
+
+            timer.Start();
+        }
+
+        private readonly Dispatcher _dispatcher;
+        private DispatcherTimer timer;
+
+        private Simulation simulation;
+
+        public SimulationViewModel Simulation => new SimulationViewModel(this.simulation);
+
+        private void CreateHunter(double x, double y)
+        {
+            this.simulation.Species[0].CreateBoid(new Vector2D(x, y));
+        }
+
+        private void CreatePrey(double x, double y)
+        {
+            this.simulation.Species[1].CreateBoid(new Vector2D(x, y));
+        }
+
+        public void ToggleTimer()
+        {
+            if (this.timer.IsEnabled)
+                this.timer.Stop();
+            else
+                this.timer.Start();
+        }
+
+        private ICommand _pause;
+        public ICommand Pause
+        {
+            get
+            {
+                return _pause ?? (_pause = new CommandHandler(() => ToggleTimer(), true));
+            }
+        }
+
+        public class CommandHandler : ICommand
+        {
+            private Action _action;
+            private bool _canExecute;
+            public CommandHandler(Action action, bool canExecute)
+            {
+                _action = action;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                _action();
+            }
+        }
+
+        public void Update(double dt)
+        {
+            this.simulation.Update(dt);
+        }
+    }
+
+    public class SimulationViewModel
+    {
+        private readonly Simulation simulation;
+
+        public SimulationViewModel(Simulation simulation)
+        {
+            this.simulation = simulation;
+        }
+
+        private World world => simulation.World;
+
+        public WorldViewModel World => new WorldViewModel(world);
+    }
+
+    public class WorldViewModel
+    {
+        private readonly World world;
+
+        public WorldViewModel(World world)
+        {
+            this.world = world;
+
+            Population = new ObservableCollection<BoidViewModel>();
+            foreach (Boid boid in population)
+                Population.Add(new BoidViewModel(boid));
+        }
+
+        private ObservableCollection<Boid> population => world.Population;
+
+        private  ObservableCollection<BoidViewModel> _population;
+        public ObservableCollection<BoidViewModel> Population
+        {
+            get { return _population; }
+            set
+            {
+                if (value == _population)
+                    return;
+
+                _population = value;
+            }
+        }
+
+        private ParameterBindings bindings => world.Bindings;
+
+        public Cell<double> Width => bindings.Read(World.Width);
+        public Cell<double> Height => bindings.Read(World.Height);
+    }
+
+    public class BoidViewModel
+    {
+        private readonly Boid boid;
+
+        public BoidViewModel(Boid boid)
+        {
+            this.boid = boid;
+        }
+
+        public ParameterBindings Bindings => boid.Bindings;
+        public World World => boid.World;
+        public Cell<Vector2D> Position => boid.Position;
+        public Cell<Vector2D> Velocity => boid.Velocity;
+        public BoidSpecies Species => boid.Species;
+        public IArtificialIntelligence AI => boid.AI;
+
+        public Cell<double> MaximumSpeed => Bindings.Read(BoidSpecies.MaximumSpeed);
+
+        public void SetMaximumSpeed()
+        {
+            MaximumSpeed.Value = BoidSpecies.MaximumSpeed.Maximum;
+        }
+
+        private ICommand _maxSpeed;
+        public ICommand MaxSpeed
+        {
+            get
+            {
+                return _maxSpeed ?? (_maxSpeed = new CommandHandler(() => SetMaximumSpeed(), true));
+            }
+        }
+
+        public class CommandHandler : ICommand
+        {
+            private Action _action;
+            private bool _canExecute;
+            public CommandHandler(Action action, bool canExecute)
+            {
+                _action = action;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public void Execute(object parameter)
+            {
+                _action();
+            }
+        }
+    }
+}
