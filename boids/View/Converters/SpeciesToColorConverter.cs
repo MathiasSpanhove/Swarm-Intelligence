e63bf74272e4f5dc60a4b81392fe6993a2369bf3
Mathias S <mathias.spanhove@hotmail.be>
2017-05-25 15:36:28 +0200

    Changed Ellipse color to GradientColorBrush to show rotation

--- boids/View/Converters/SpeciesToColorConverter.cs
+++ boids/View/Converters/SpeciesToColorConverter.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace View
 {
@@ -12,10 +14,17 @@ namespace View
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            RadialGradientBrush myBrush = new RadialGradientBrush();
+            myBrush.GradientOrigin = new Point(0.5, 0.75);
+            myBrush.GradientStops.Add(new GradientStop(Colors.Yellow, 0.0));
+            myBrush.GradientStops.Add(new GradientStop(Colors.Orange, 0.3));
+
             if (value is HunterSpecies)
-                return "Red";
+                myBrush.GradientStops.Add(new GradientStop(Colors.Red, 1.0));
             else
-                return "Green";
+                myBrush.GradientStops.Add(new GradientStop(Colors.Green, 1.0));
+
+            return myBrush;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
